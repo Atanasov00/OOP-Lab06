2e7086cb79871fae28f1412c45f4bb1bfb2ad861
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2020-11-16 00:40:04 +0100

    Completed the exercise 1

--- src/it/unibo/oop/lab/collections1/UseCollection.java
+++ src/it/unibo/oop/lab/collections1/UseCollection.java
@@ -1,11 +1,22 @@
 package it.unibo.oop.lab.collections1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Example class using {@link java.util.List} and {@link java.util.Map}.
  * 
  */
 public final class UseCollection {
 
+	private static final int START_NUMBER = 1_000;
+	private static final int LAST_NUMBER = 1_999;
+	private static final int NEW_ELEMENTS = 1_000_00;
+	private static final int TO_MS = 1_000_000;
+	
     private UseCollection() {
     }
 
@@ -63,5 +74,77 @@ public final class UseCollection {
         /*
          * 8) Compute the population of the world
          */
+    	
+    	var arrayList = new ArrayList<Integer>();
+    	
+    	for(int i = START_NUMBER; i <= LAST_NUMBER; i++) {
+    		arrayList.add(i);
+    	}
+    	
+    	var linkedList = new LinkedList<Integer>(arrayList);
+    	
+    	int temporaryVar = arrayList.get(0);
+    	arrayList.set(0, arrayList.get(arrayList.size() - 1));
+    	arrayList.set(arrayList.size() - 1, temporaryVar);
+    	
+    	for(Integer i: arrayList) {
+    		System.out.println(i);
+    	}
+    	
+    	long time = System.nanoTime();
+    	Random rd = new Random();
+    	
+    	for(int i = 0; i < NEW_ELEMENTS; i++) {
+    		arrayList.add(0, rd.nextInt());
+    	}
+    	
+    	time = System.nanoTime() - time;
+    	
+    	System.out.println("Measurement of ArrayList: " + time/TO_MS + "ms");
+    	
+    	time = System.nanoTime();
+    	
+    	for(int i = 0; i < NEW_ELEMENTS; i++) {
+    		linkedList.add(0, rd.nextInt());
+    	}
+    	
+    	time = System.nanoTime() - time;
+    	
+    	System.out.println("Measurement of LinkedList: " + time/TO_MS + "ms");
+    	
+    	long time2 = System.nanoTime();
+    	time = System.nanoTime();
+    	
+    	for(int i = 0; i < START_NUMBER; i++) {
+    		System.out.println(arrayList.get(arrayList.size()/2));
+    	}
+    	
+    	time = System.nanoTime() - time; 
+    	
+    	for(int i = 0; i < START_NUMBER; i++) {
+    		System.out.println(linkedList.get(linkedList.size()/2));
+    	}
+    	
+    	time2 = System.nanoTime() - time2;
+    	
+    	
+    	System.out.println("Reading Measurement of ArrayList: " + time +
+    						"\nReading Measurement of LinkedList: " + time);
+    	
+    	Map<String, Double> map = new HashMap<>();
+    	
+    	map.put("Africa", 1110635000.0);
+    	map.put("Americas", 972005000.0);
+    	map.put("Asia", 4298723000.0);
+    	map.put("Europe", 742452000.0);
+    	map.put("Oceania", 38304000.0);
+    	map.put("Antartica", 0.0);
+    	
+    	double sum = 0;
+    	for(final Double d: map.values()) {
+    		sum += d;
+    	}
+    	
+    	System.out.println("The population is about: " + sum);
     }
 }
